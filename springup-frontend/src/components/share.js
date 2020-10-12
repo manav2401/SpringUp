96b4752fb66a892910b821c2770ad145839f623e
t-proctor <tim.proctor75@gmail.com>
2020-10-11 22:34:00 -0500

    Basis share

--- springup-frontend/src/components/share.js
+++ springup-frontend/src/components/share.js
@@ -27,9 +27,9 @@ function Share({ title, desc, ...rest }) {
         shadow = "md"
         
       >
-        <Heading padding='20px' textAlign='center' paddingBottom='5px'>Fund your creations, reward your audience</Heading>
-        <Text padding='20px' paddingTop='5px' textAlign='center'>        
-        Independent creators deal with censorship, high fees, and poor audience engagement with the present funding models. With SpringUp, creators and supporters don't have these problems. Creators get censorship free, low fee funding. Supporters get perks based on how early they supported, how much they interact, and how much they give. Creators early on in their career benefit way more now. 
+        <Heading padding='20px' textAlign='center' paddingBottom='5px'>Share your page</Heading>
+        <Text padding='20px' paddingTop='5px' textAlign='center'> 
+        springup.eth/e10m2       
         </Text>
         {/* <Heading size="md" padding='20px' as="h2">
         Independent creators deal with censorship, high fees, and poor audience engagement with the present funding models. With SpringUp, creators and supporters don't have these problems. Creators get censorship free, low fee funding. Supporters get perks not entirely based on the amount of money given
